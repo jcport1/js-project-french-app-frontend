7955b7bfb215eabbe2e44c91266833872bd9b705
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-02-18 16:37:55 -0500

    Revises Create New Verb Form

--- src/components/verb.js
+++ src/components/verb.js
@@ -2,11 +2,12 @@ class Verb {
     constructor(data) {
         this.id = data.id;
         this.name = data.name;
+        this.translation = data.translation 
         Verb.all.push(this);
     }
 
     renderVerb(){
-        return `<ul><h3>${this.name}</h3></ul>`
+        return `<ul><h3>${this.name} - ${this.translation}</h3></ul>`
     }
 }
 
